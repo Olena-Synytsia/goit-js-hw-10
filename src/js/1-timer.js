de2a43cd393842298a58c882680b752f2e7d244e
alena_sinitsa <alenasinitsa85@gmail.com>
2024-07-20 21:56:24 +0300

    end

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -16,7 +16,6 @@ let timerInterval = 0;
 
 const options = {
   enableTime: true,
-  dateFormat: 'Y-m-d H:i',
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,4 +6,32 @@ formEl.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(event) {
   event.preventDefault();
+
+  const delay = formEl.delay.value;
+  const state = formEl.state.value;
+
+  const createPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  createPromise.then(
+    delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    },
+    delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    }
+  );
 }
