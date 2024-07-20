853d8cc94c1b56a498c64f342d6d8fc9868ca377
alena_sinitsa <alenasinitsa85@gmail.com>
2024-07-20 18:14:29 +0300

    snackbar js start

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,9 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formEl = document.querySelector('form');
+formEl.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(event) {
+  event.preventDefault();
+}
