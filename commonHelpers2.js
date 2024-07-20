649c61e1b7154814f600c92a9525daebeb1b00da
Olena-Synytsia <160421916+Olena-Synytsia@users.noreply.github.com>
2024-07-20 18:57:00 +0000

    Deploying to gh-pages from @ Olena-Synytsia/goit-js-hw-10@de2a43cd393842298a58c882680b752f2e7d244e 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import"./assets/vendor-77e16229.js";const e=document.querySelector("form");e.addEventListener("submit",r);function r(t){t.preventDefault()}
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as r}from"./assets/vendor-77e16229.js";const s=document.querySelector("form");s.addEventListener("submit",l);function l(i){i.preventDefault();const t=s.delay.value,o=s.state.value;new Promise((e,m)=>{setTimeout(()=>{o==="fulfilled"?e(t):m(t)},t)}).then(e=>{r.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})},e=>{r.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})}
 //# sourceMappingURL=commonHelpers2.js.map
